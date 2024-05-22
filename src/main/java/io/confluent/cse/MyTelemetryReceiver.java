f2c2d54e398d478a969f4ae05225bcaa82031494
Alex Bleasdale <ableasdale@confluent.io>
2024-05-22 15:09:32 +0100

    Added some logging in my custom class

--- src/main/java/io/confluent/cse/MyTelemetryReceiver.java
+++ src/main/java/io/confluent/cse/MyTelemetryReceiver.java
@@ -5,31 +5,35 @@ import org.apache.kafka.common.metrics.MetricsReporter;
 import org.apache.kafka.server.authorizer.AuthorizableRequestContext;
 import org.apache.kafka.server.telemetry.ClientTelemetryPayload;
 import org.apache.kafka.server.telemetry.ClientTelemetryReceiver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Map;
 
 public class MyTelemetryReceiver implements MetricsReporter {
 
+    private static final Logger log = LoggerFactory.getLogger(MyTelemetryReceiver.class);
+
     @Override
     public void init(List<KafkaMetric> list) {
-        System.out.println("***** DOING SOME STUFF!! *******");
-        System.out.println("***** HEY LOOK, I AM DOING SOME STUFF!! *******");
+        log.info("*** MyTelemetryReceiver :: init() ***");
     }
 
     @Override
     public void metricChange(KafkaMetric kafkaMetric) {
-
+        log.info("**** metricChange detected ****");
+        log.info("** metricChange - name: "+kafkaMetric.metricName() + " - value: - " + kafkaMetric.metricValue().toString());
     }
 
     @Override
     public void metricRemoval(KafkaMetric kafkaMetric) {
-
+        log.info("***** bye then! *****");
     }
 
     @Override
     public void close() {
-
+        log.info("******************** CLOSE *************************");
     }
 
     @Override
